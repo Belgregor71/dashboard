ba27fe9ee064386fa878a8967de8fb8f51469ea3
Belgregor71 <126452350+Belgregor71@users.noreply.github.com>
2026-01-17 16:06:03 +1000

    Update HA host IP address in configuration

--- config/ha.js
+++ config/ha.js
@@ -1,4 +1,4 @@
 export const HA_CONFIG = {
-  host: "http://192.168.1.10:8123", // Synology IP
+  host: "http://192.168.0.179:8123", // Synology IP
   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJlMDZkYWJiNGZhMGY0YThhOTlhZGU1MTkzZTg2YmFkZiIsImlhdCI6MTc2ODIwMTEwNiwiZXhwIjoyMDgzNTYxMTA2fQ.4jTFUKZYqaf90O_whd5I8v5MItBhIhLbUxOQhOhjWbI"
 };
