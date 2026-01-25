9b179674c01ff16fbb6d16bcc0c4945f4d556619
Belgregor71 <126452350+Belgregor71@users.noreply.github.com>
2026-01-25 17:57:27 +1000

    Add click-to-cycle dashboard views (#35)

--- static/js/core/viewManager.js
+++ static/js/core/viewManager.js
@@ -1,9 +1,29 @@
 import { emit } from "./eventBus.js";
 
 let currentView = "home";
+const viewOrder = ["home", "cameras", "calendar", "agenda"];
+let clickHandlerRegistered = false;
+
+function getNextView(view) {
+  const index = viewOrder.indexOf(view);
+  if (index === -1) return viewOrder[0];
+  return viewOrder[(index + 1) % viewOrder.length];
+}
+
+function registerClickCycle() {
+  if (clickHandlerRegistered) return;
+  clickHandlerRegistered = true;
+
+  document.addEventListener("click", event => {
+    if (event.defaultPrevented) return;
+    const nextView = getNextView(currentView);
+    switchView(nextView);
+  });
+}
 
 export function initViews() {
   document.body.dataset.view = currentView;
+  registerClickCycle();
 }
 
 export function switchView(view) {
